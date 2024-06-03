2f08926c47982756344476b6da2b3794c798f5db
clément perrousset <clement.perrousset@gmail.com>
2024-06-03 18:19:30 +0200

    Initial commit

---
+++ Site_aide_visite/__init__.py
@@ -0,0 +1,3 @@
+"""
+Package for Site_aide_visite.
+"""
---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\D\projet_site_aide_visite\Site aide visite\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
